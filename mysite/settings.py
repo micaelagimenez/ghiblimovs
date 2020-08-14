3bf3626c43344bf7d509fee1e5948dcae1e5c6f7
Micaela <micaelagimenez@live.com.ar>
2020-08-14 19:22:27 -0300

    initial heroku commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
