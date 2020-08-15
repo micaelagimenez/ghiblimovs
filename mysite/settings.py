20b8201adf26811cdc5ada042a7fbaa30916f139
Micaela <micaelagimenez@live.com.ar>
2020-08-15 11:07:07 -0300

    heroku updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
      ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICSTORAGE = "Whitenoise.storage.CompressedManifestStaticFilesStorage"
